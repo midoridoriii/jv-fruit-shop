bb2c2c15f00d80b1331ffdf3db2414a73fea2ee9
midoridoriii <valissa069@gmail.com>
2025-12-25 09:32:03 +0200

    added file writer implementation

---
+++ src/main/java/core/basesyntax/FileWriter.java
@@ -0,0 +1,7 @@
+package core.basesyntax;
+
+import java.util.List;
+
+public interface FileWriter {
+    public void write(List<String> lines, String path);
+}
---
+++ src/main/java/core/basesyntax/FileWriterImpl.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class FileWriterImpl implements FileWriter {
+    @Override
+    public void write(List<String> lines, String path) {
+        if (lines == null) {
+            throw new RuntimeException("Lines can`t be null!");
+        }
+        if (path == null) {
+            throw new RuntimeException("Path can`t be null!");
+        }
+
+        try {
+            Files.write(Path.of(path), lines);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot write file: " + path, e);
+        }
+    }
+}
